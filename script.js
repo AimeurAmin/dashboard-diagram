6cb98372d7291f329a7ff29a7006196d3d76f5a0
Aimeur Amin <mohammedamin.aimeur@gmail.com>
2022-09-03 04:04:20 +0100

    changes

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 var baseurl = 'http://127.0.0.1:8000'
 // CommentModel/
 
-const getAllComments = async () => {
+async function getAllComments() {
     var response = null
-    await fetch(baseurl + '/CommentModel/').then(async (data) => {
+    await fetch(baseurl + '/CommentModelShow/').then(async (data) => {
 		response = await data.json();
 	}).catch(e => {
 		console.error('something went wrong with the request to the classifier! => check the log below: \n' + e);
@@ -12,7 +12,7 @@ const getAllComments = async () => {
     return response;
 }
 
-const getAllWilayas = async () => {
+async function getAllWilayas() {
     var response = null
     await fetch(baseurl + '/CityModel/').then(async (data) => {
 		response = await data.json();
@@ -23,9 +23,10 @@ const getAllWilayas = async () => {
     return response
 }
 
-getAllComments();
 
-const getClassificationByWilaya = async (wilaya_code) => {
+var allClassifications = [];
+
+const getClassificationByWilaya = async () => {
     const allComments = await getAllComments();
     const wilayas = await getAllWilayas();
     const wilayasClassifications = {}
@@ -39,9 +40,9 @@ const getClassificationByWilaya = async (wilaya_code) => {
     allComments.forEach(comment => {
         if(comment.calssification === 'positif') {
             wilayasClassifications[comment.user.city.city_name]['positif'] = (wilayasClassifications[comment.user.city.city_name].positif | 0) + 1
-        }
-        if(comment.classification === 'else') {
-            wilayasClassifications[comment.user.city.city_name].negatif = (wilayasClassifications[comment.user.city.city_name].positif | 0) + 1
+        } else  {
+            console.log((wilayasClassifications[comment.user.city.city_name].negatif | 0) + 1);
+            wilayasClassifications[comment.user.city.city_name]['negatif'] = (wilayasClassifications[comment.user.city.city_name].negatif | 0) + 1
         }
     })
 
@@ -50,217 +51,81 @@ const getClassificationByWilaya = async (wilaya_code) => {
 
 
 
-const maxPercentage = 65
-const CSATs = [{
-    wilaya: 'Adrar',
-    CSAT: 25
-}, {
-    wilaya: 'Chlef',
-    CSAT: 45
-}, {
-    wilaya: 'Alger',
-    CSAT: 65
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-},{
-    wilaya: 'Adrar',
-    CSAT: 25
-}, {
-    wilaya: 'Chlef',
-    CSAT: 45
-}, {
-    wilaya: 'Alger',
-    CSAT: 65
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-},{
-    wilaya: 'Adrar',
-    CSAT: 25
-}, {
-    wilaya: 'Chlef',
-    CSAT: 45
-}, {
-    wilaya: 'Alger',
-    CSAT: 65
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-},{
-    wilaya: 'Adrar',
-    CSAT: 25
-}, {
-    wilaya: 'Chlef',
-    CSAT: 45
-}, {
-    wilaya: 'Alger',
-    CSAT: 65
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-},{
-    wilaya: 'Adrar',
-    CSAT: 25
-}, {
-    wilaya: 'Chlef',
-    CSAT: 45
-}, {
-    wilaya: 'Alger',
-    CSAT: 65
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-},{
-    wilaya: 'Adrar',
-    CSAT: 25
-}, {
-    wilaya: 'Chlef',
-    CSAT: 45
-}, {
-    wilaya: 'Alger',
-    CSAT: 65
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-},{
-    wilaya: 'Adrar',
-    CSAT: 25
-}, {
-    wilaya: 'Chlef',
-    CSAT: 45
-}, {
-    wilaya: 'Alger',
-    CSAT: 65
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-},{
-    wilaya: 'Adrar',
-    CSAT: 25
-}, {
-    wilaya: 'Chlef',
-    CSAT: 45
-}, {
-    wilaya: 'Alger',
-    CSAT: 65
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-},{
-    wilaya: 'Adrar',
-    CSAT: 25
-}, {
-    wilaya: 'Chlef',
-    CSAT: 45
-}, {
-    wilaya: 'Alger',
-    CSAT: 65
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-},{
-    wilaya: 'Adrar',
-    CSAT: 25
-}, {
-    wilaya: 'Chlef',
-    CSAT: 45
-}, {
-    wilaya: 'Alger',
-    CSAT: 65
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-},{
-    wilaya: 'Adrar',
-    CSAT: 25
-}, {
-    wilaya: 'Chlef',
-    CSAT: 45
-}, {
-    wilaya: 'Alger',
-    CSAT: 65
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-}, {
-    wilaya: 'Oran',
-    CSAT: 55
-},
-]
+
+var maxPercentage = 0
+
 var numberOfLines = Math.ceil(maxPercentage / 10);
-console.log('here');
 var container = document.getElementById('parameter')
 
-
-for (let i = 1; i <= numberOfLines; i++) {
-    var line = document.createElement('div')
-    line.classList.add('line')
-    line.style.bottom = (i*(100 / numberOfLines)) + '%'
-    container.appendChild(line)
-}
-
 var spacing = 1
-CSATs.forEach(element => {
-    var barContainer = document.createElement('div');
-    var bar = document.createElement('div');
-    var csat = document.createElement('div');
-    var wilayaName = document.createElement('div');
 
-    barContainer.classList.add('bar-container');
-    bar.classList.add('bar');
-    csat.classList.add('csat');
-    wilayaName.classList.add('wilaya-name')
-    
-    barContainer.style.left = spacing + '%';
-    barContainer.style.height = parseInt(element.CSAT) * 100 / (numberOfLines * 10) + '%';
-    if(element.CSAT >= 65) {
-        bar.style.backgroundColor = '#39b150';
-    } else if(element.CSAT >= 60) {
-        bar.style.backgroundColor = '#92d050';
-    } else if (element.CSAT >= 55) {
-        bar.style.backgroundColor = '#92d050';
-    } else if (element.CSAT < 55) {
-        bar.style.backgroundColor = '#f51d03';
+async function calculateCSATs() {
+    const wilayasClassifications = await getClassificationByWilaya();
+    const wilayas = await getAllWilayas();
+    console.log(wilayasClassifications);
+    console.log(wilayas);
+    const CSATs = []
+    wilayas.forEach(wilaya => {
+        const positif =  wilayasClassifications[wilaya.city_name].positif
+        const total = positif + wilayasClassifications[wilaya.city_name].negatif
+        const CSAT = Math.floor((total ? positif / total : 0) * 100)
+        console.log(positif);
+        console.log(positif / (total | 1));
+        console.log(CSAT);
+        if(CSAT > maxPercentage) {
+            maxPercentage = CSAT
+        }
+        CSATs.push({
+            wilaya: wilaya.city_name,
+            CSAT
+        })
+    });
+    console.log(CSATs);
+    return CSATs
+}
+async function visualizeCharts() {
+    const CSATs = await calculateCSATs();
+    var numberOfLines = Math.ceil(maxPercentage / 10);
+
+
+    for (let i = 1; i <= numberOfLines; i++) {
+        var line = document.createElement('div')
+        line.classList.add('line')
+        line.style.bottom = (i*(100 / numberOfLines)) + '%'
+        container.appendChild(line)
     }
-    spacing += 1.8;
+    CSATs.forEach(element => {
+        var barContainer = document.createElement('div');
+        var bar = document.createElement('div');
+        var csat = document.createElement('div');
+        var wilayaName = document.createElement('span');
     
-    csat.innerHTML = element.CSAT
-    wilayaName.innerHTML = element.wilaya
-
-    barContainer.appendChild(bar);
-    barContainer.appendChild(csat);
-    barContainer.appendChild(wilayaName);
-    container.appendChild(barContainer);
-});
+        barContainer.classList.add('bar-container');
+        bar.classList.add('bar');
+        csat.classList.add('csat');
+        wilayaName.classList.add('wilaya-name')
+        
+        barContainer.style.left = spacing + '%';
+        barContainer.style.height = ((parseInt(element.CSAT) * 100 )/ (numberOfLines * 10)) + '%';
+        if(element.CSAT >= 65) {
+            bar.style.backgroundColor = '#39b150';
+        } else if(element.CSAT >= 60) {
+            bar.style.backgroundColor = '#92d050';
+        } else if (element.CSAT >= 55) {
+            bar.style.backgroundColor = '#92d050';
+        } else if (element.CSAT < 55) {
+            bar.style.backgroundColor = '#f51d03';
+        }
+        spacing += 2;
+        
+        csat.innerHTML = element.CSAT
+        wilayaName.innerHTML = element.wilaya
+    
+        barContainer.appendChild(bar);
+        barContainer.appendChild(csat);
+        barContainer.appendChild(wilayaName);
+        container.appendChild(barContainer);
+    });
+}
+visualizeCharts();
 
